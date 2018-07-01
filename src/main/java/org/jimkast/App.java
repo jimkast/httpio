2c39ec39ff2f4129826ecd288619948d4df41014
jimkast <jimkast.ath@gmail.com>
2018-07-02 02:49:21 +0300

    tcp no delay

--- src/main/java/org/jimkast/App.java
+++ src/main/java/org/jimkast/App.java
@@ -4,10 +4,11 @@ import java.io.IOException;
 import org.jimkast.http.bk.BkBasic;
 import org.jimkast.http.bk.BkCloseStreams;
 import org.jimkast.http.bk.BkRetry;
+import org.jimkast.http.bk.BkTcpNoDelay;
 import org.jimkast.http.bk.BxBuffered;
 import org.jimkast.http.bk.BxHttp;
 import org.takes.http.BkParallel;
-import org.takes.http.BkSafe;
+import org.takes.http.BkTimeable;
 import org.takes.http.Exit;
 import org.takes.http.FtBasic;
 
@@ -15,19 +16,23 @@ public final class App {
     public static void main(String... args) throws IOException {
         new FtBasic(
             new BkParallel(
-                new BkSafe(
-                    new BkCloseStreams(
-                        new BkRetry(
-                            1000,
-                            new BkBasic(
-                                new BxBuffered(
-                                    new BxHttp(
-                                        new TkApp()
+                new BkTimeable(
+//                    new BkSafe(
+                    new BkTcpNoDelay(
+                        new BkCloseStreams(
+                            new BkRetry(5000,
+                                new BkBasic(
+                                    new BxBuffered(
+                                        new BxHttp(
+                                            new TkApp()
+                                        )
                                     )
                                 )
                             )
+//                            )
                         )
-                    )
+
+                    ), 30000
                 )
             ),
             9999
---
+++ src/main/java/org/jimkast/http/bk/BkTcpNoDelay.java
@@ -0,0 +1,19 @@
+package org.jimkast.http.bk;
+
+import java.io.IOException;
+import java.net.Socket;
+import org.takes.http.Back;
+
+public final class BkTcpNoDelay implements Back {
+    private final Back origin;
+
+    public BkTcpNoDelay(Back origin) {
+        this.origin = origin;
+    }
+
+    @Override
+    public void accept(Socket socket) throws IOException {
+        socket.setTcpNoDelay(true);
+        origin.accept(socket);
+    }
+}
